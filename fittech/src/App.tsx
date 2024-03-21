ec1b17239e0b06cc74b8a74fb3770df591202e75
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-03-21 15:54:34 +0100

    ..

--- fittech/src/App.tsx
+++ fittech/src/App.tsx
@@ -1,9 +1,15 @@
 import Navbar from "./scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 
 function App() {
-  return <div className="app bg-gray-20">
-    <Navbar />
-  </div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} page={""} />
+    </div>
+  );
 }
-
+ 
 export default App;
